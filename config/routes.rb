a38ff77a6e64b18faa272099582811b922ba5147
Giovanni Malesci <giovanni.malesci@hotmail.com>
2023-11-21 17:02:27 +0100

    Last push for today

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,10 @@ Rails.application.routes.draw do
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get 'up' => 'rails/health#show', as: :rails_health_check
   get '/tasks', to: 'tasks#index', as: 'tasks'
-  get '/tasks/:id', to: 'tasks#show', as: 'task'
   get 'tasks/new', to: 'tasks#new', as: 'new_task'
+  get '/tasks/:id', to: 'tasks#show', as: 'task'
   post 'tasks', to: 'tasks#create'
-
+  get 'tasks/:id/edit', to: 'tasks#edit', as: 'edit_task'
 
   # Defines the root path route ("/")
   # root "posts#index"
